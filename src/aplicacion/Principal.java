fab8010d2d4b9090a83c744dfcebfd21c9d327fd
Álvaro <ainiguezdislaa@gmail.com>
2023-02-20 15:26:54 +0100

    ej7 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,15 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.Scanner;
+import dominio.ej2_7;
+
+
+public class Principal {
+    public static void main(String[]args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Dime un numero ");
+        int n=sc.nextInt();
+        System.out.println("La suma de los números pares hasta "+n+" es igual a "+ej2_7.sumapares(n));
+
+    }
+
 }
--- src/dominio/ej2_7.java
+++ src/dominio/ej2_7.java
@@ -1,2 +1,27 @@
-package dominio;public class ej2_7 {
+package dominio;
+
+public class ej2_7 {
+
+    public static int sumapares(int n) {
+
+        if(n%2!=0) {
+            n=n-1;
+        }
+
+        if(n==0) {
+            return n;
+
+
+
+        }else {
+            return n+sumapares(n-2);
+        }
+
+    }
+
+
 }
+
+
+
+
